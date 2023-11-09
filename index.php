2d5a6576e75573eda29c4013f5d9b163cd04afc0
robertocannella <robertocannella@gmail.com>
2023-11-08 20:33:16 -0500

    Extract all headers into an array

--- index.php
+++ index.php
@@ -6,24 +6,42 @@ $headers = [
     "Authorization: Client-ID GKjyF4RHv8V3zEyr4lZyNQ0PsLN3jgyzMBvNLqtIzRw"
 ];
 
+$response_headers = [];
+
+/*
+ * This closure callback to be called by CURLOPT_HEADERFUNCTION
+ */
+$header_callback = function ($ch,$header) use(&$response_headers) { // &= pass by reference
+
+    $len = strlen($header); // get the length of the header
+
+    $parts = explode(':',$header,2); // max 2 parts
+
+    if ( count($parts) < 2 ){ // ignore invalid headers (less than 2 parts)
+        return $len;
+    }
+
+    $response_headers[$parts[0]] = trim($parts[1]); // push the header k/v pair into the header array
+
+    return $len; // for now, just return the len
+
+};
+
+
 curl_setopt_array($ch, [
     CURLOPT_URL => "https://api.unsplash.com/photos",
     CURLOPT_RETURNTRANSFER => true, //true to return the transfer as a string of the return value of curl_exec() instead of outputting it directly.
     CURLOPT_HTTPHEADER => $headers, // An array of HTTP header fields to set, in the format array('Content-type: text/plain', 'Content-length: 100')
-    CURLOPT_HEADER => true  // 	true to include the header in the output.
+    CURLOPT_HEADERFUNCTION => $header_callback
 ]);
 
 $response = curl_exec($ch); // execute the request, saving the response
 
 $status_code = curl_getinfo($ch,CURLINFO_RESPONSE_CODE); // Integer Status code
 
-$content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-
-$content_length = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
 
 curl_close($ch);
 
 echo $status_code . "\n";
-echo $content_type . "\n";
-echo $content_length . "\n";
+print_r($response_headers);
 echo $response . "\n";
