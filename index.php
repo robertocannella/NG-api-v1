113a22ba5825157e720b2f5f385f29d8a5add8f3
robertocannella <robertocannella@gmail.com>
2023-11-08 21:44:44 -0500

    Add Guzzle HTTP Library

--- index.php
+++ index.php
@@ -1,32 +1,29 @@
 <?php
 
-$ch = curl_init();
+use GuzzleHttp\Exception\GuzzleException;
 
-$headers = [
-    "Authorization: token SECRET+KEY",
-   // "User-Agent: robertocannella"
-];
+require __DIR__ . "/vendor/autoload.php";
 
-$payload = json_encode([
-    "name" => "Create Via API",
-    "description" => "A repository created via API"
-]);
+$client = new GuzzleHttp\Client;
 
-curl_setopt_array($ch, [
-    CURLOPT_URL => "https://api.github.com/user/repos",
-    CURLOPT_RETURNTRANSFER => true, //true to return the transfer as a string of the return value of curl_exec() instead of outputting it directly.
-    CURLOPT_HTTPHEADER => $headers, // An array of HTTP header fields to set, in the format array('Content-type: text/plain', 'Content-length: 100')
-    CURLOPT_USERAGENT => 'robertocannella',
-    // CURLOPT_CUSTOMREQUEST => 'POST',  // optional when using POSTFIELDS
-    // CURLOPT_POST => true,             // alternative to above, optional when using POSTFIELDS
-    CURLOPT_POSTFIELDS => $payload
-]);
+try {
+   $response =  $client->request("GET", "https://api.github.com/user/repos",[
+        "headers" => [
+            "Authorization: Bearer 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",
+            "User-Agent: RobertoCannella",
+        ],
+       "debug" => true // dump all headers
+    ]);
 
-$response = curl_exec($ch); // execute the request, saving the response
+} catch (GuzzleException $e) {
+    echo $e->getMessage();
+}
+if (isset($response)) {
+    print_r($response->getHeaders());
+    print_r($response->getBody());
+} else {
+    echo "No response.\n";
+}
 
-$status_code = curl_getinfo($ch,CURLINFO_RESPONSE_CODE); // Integer Status code
 
-curl_close($ch);
 
-echo $status_code . "\n";
-echo $response . "\n";
