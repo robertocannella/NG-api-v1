809f3b3ea6d0f60dd8a80994a4573a97be5438db
robertocannella <robertocannella@gmail.com>
2023-11-08 21:02:55 -0500

    Add Custome Request Header

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $ch = curl_init();
 
 $headers = [
-    "Authorization: Bearer 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",
+    "Authorization: token YOUR+SECRET+KEY",
    // "User-Agent: robertocannella"
 ];
 
@@ -11,7 +11,8 @@ curl_setopt_array($ch, [
     CURLOPT_URL => "https://api.github.com/user/starred/httpie/cli",
     CURLOPT_RETURNTRANSFER => true, //true to return the transfer as a string of the return value of curl_exec() instead of outputting it directly.
     CURLOPT_HTTPHEADER => $headers, // An array of HTTP header fields to set, in the format array('Content-type: text/plain', 'Content-length: 100')
-    CURLOPT_USERAGENT => 'robertocannella'
+    CURLOPT_USERAGENT => 'robertocannella',
+    CURLOPT_CUSTOMREQUEST => 'PUT'
 ]);
 
 $response = curl_exec($ch); // execute the request, saving the response
