b4baba824d09cd0b645a874070ddc715fed37d92
robertocannella <robertocannella@gmail.com>
2023-11-10 14:00:32 -0500

    Refactor webroot. ADD PDO connection class: Database.php

--- api/index.php
+++ api/index.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
-require dirname(__DIR__) . "/v1/vendor/autoload.php";
+require dirname(__DIR__) . "/vendor/autoload.php";
 
 set_exception_handler([ErrorHandler::class, 'handleException']);
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); // Exclude any parameters
 
@@ -24,6 +27,10 @@ if ($resource != 'tasks'){
 }
 
 header("Content-type: application/json; charset: UTF-8");
+
+$db = new Database($_ENV["DB_HOST"],$_ENV["DB_NAME"],$_ENV["DB_USER"],$_ENV["DB_PASS"]);
+
+$db->getConnection();
 
 $controller = new TaskController();
 
