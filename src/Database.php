2e26953a6ed5fc89e4b2b8a804cf1900608ac036
robertocannella <robertocannella@gmail.com>
2023-11-12 07:45:59 -0500

    Cache DataBase Connection

--- src/Database.php
+++ src/Database.php
@@ -1,21 +1,28 @@
 <?php
 
 class Database {
+
+    private ?PDO $conn = null;
+
     public function __construct(
-        private string $host,
-        private string $name,
-        private string $user,
-        private string $pass)
+        private readonly string $host,
+        private readonly string $name,
+        private readonly string $user,
+        private readonly string $pass)
     {
     }
     public function getConnection () : PDO {
 
         $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
 
-        return new PDO($dsn, $this->user,$this->pass,[
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Throw exceptions
-            PDO::ATTR_STRINGIFY_FETCHES => false, // Keep numeric values numeric
-            PDO::ATTR_EMULATE_PREPARES => false // Maria DB/MYSQL support this so it can be disabled
-        ]);
+        if ( $this->conn === null ) {
+            $this->conn =  new PDO($dsn, $this->user,$this->pass,[
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Throw exceptions
+                PDO::ATTR_STRINGIFY_FETCHES => false, // Keep numeric values numeric
+                PDO::ATTR_EMULATE_PREPARES => false // Maria DB/MYSQL support this so it can be disabled
+            ]);
+        }
+
+        return $this->conn;
     }
 }
