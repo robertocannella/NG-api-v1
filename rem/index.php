78e9f6f6bb112b63218d19ddec48a6606dfa3dda
robertocannella <robertocannella@gmail.com>
2023-11-23 18:27:10 -0500

    Add getPatternFromRoutePath() in Router.php

--- rem/index.php
+++ rem/index.php
@@ -15,9 +15,13 @@ $path = str_replace($home_dir,"", $path);
 
 
 $router = new Router();
+
+$router->add("/product/{slug:[\w-]+}", ["controller" => "products", "action" => "show"]);
+$router->add("{controller}/{id:\d+}/{action}");
 $router->add("/home/index", ["controller" => "home", "action" => "index"]);
 $router->add("/products", ["controller" => "products", "action" => "index"]);
 $router->add("/",["controller" => "home", "action" => "index"]);
+$router->add("/{controller}/{action}");
 
 $params = $router->match($path);
 
--- rem/src/Framework/Router.php
+++ rem/src/Framework/Router.php
@@ -9,7 +9,7 @@ class Router {
     {
 
     }
-    public function add(string $path, ?array $params): void
+    public function add(string $path, array $params = []): void
     {
         $this->routes[] = [
             "path" => $path,
@@ -18,14 +18,52 @@ class Router {
     }
     public function match(string $path): bool|array
     {
+        $path = trim($path, "/");
+
         foreach ($this->routes as $route){
 
-            if ($route["path"] === $path){
+            //$pattern = "#^/(?P<controller>[a-z]+)/(?P<action>[a-z+]+)$#";
 
-                return $route["params"];
+            $pattern = $this->getPatternFromRoutePath($route["path"]);
+            //echo $pattern, "\n";
+
+            if (preg_match($pattern, $path,$matches)){
+
+                $matches = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
+
+                return array_merge($matches,$route["params"]);
+
             }
         }
+
         return false;
+
+    }
+    public function getPatternFromRoutePath (string $path): string{
+
+        $path = trim($path, "/");
+
+        $segments = explode("/", $path);
+
+        $segments = array_map(function (string $segment):string {
+
+            if (preg_match("#^\{([a-z][a-z0-9]*)\}$#", $segment, $matches)){
+
+                return "(?P<" . $matches[1] . ">[^/]+)";
+
+            }
+            if (preg_match("#^\{([a-z][a-z0-9]*):(.+)\}$#", $segment, $matches)){
+
+                return "(?P<" . $matches[1] . ">". $matches[2] . ")";
+
+            }
+
+            return $segment;
+
+        }, $segments);
+
+        return "#^" .  implode("/", $segments) . "$#iu"; // ignore case, utf-8 encoding
+
 
     }
 }
